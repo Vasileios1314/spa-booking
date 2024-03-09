8edb5be425c1306701c4cdb262567ee919ca289c
Vasileios1314 <vasilis.bobonis@hotmail.com>
2024-03-09 15:20:40 +0100

    update email

--- pages/api/route.ts
+++ pages/api/route.ts
@@ -1,23 +1,21 @@
 import { Resend } from "resend";
-import index from "@/app/emails/index";
+// import index from "@/emails/index";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export default async function POST(request, response) {
-  const { name, email, message } = await request.body;
-
-  try {
-    const data = await resend.emails.send({
-      from: process.env.NEXT_PUBLIC_EMAIL_FROM, //create a domain email
-      // from: "onboarding@resend.dev", //dev perpose
-      to: process.env.NEXT_PUBLIC_EMAIL_TO,
-      subject: "New Ticket!!",
-      react: index({ name, email, message }),
-    });
-
-    response.status(200).json({ success: true, data });
-  } catch (error) {
-    // console.error("Error sending email:", error);
-    response.status(500).json({ success: false, error: error.message });
-  }
+  // const { name, email, message } = await request.body;
+  // try {
+  //   const data = await resend.emails.send({
+  //     from: process.env.NEXT_PUBLIC_EMAIL_FROM, //create a domain email
+  //     // from: "onboarding@resend.dev", //dev perpose
+  //     to: process.env.NEXT_PUBLIC_EMAIL_TO,
+  //     subject: "New Ticket!!",
+  //     react: index({ name, email, message }),
+  //   });
+  //   response.status(200).json({ success: true, data });
+  // } catch (error) {
+  //   // console.error("Error sending email:", error);
+  //   response.status(500).json({ success: false, error: error.message });
+  // }
 }
