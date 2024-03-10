00d09bcc53ff815af9dec59da099119b19087ff5
Vasileios1314 <vasilis.bobonis@hotmail.com>
2024-03-10 10:57:35 +0100

    update styles

--- components/Apartments/ApartmentsList.tsx
+++ components/Apartments/ApartmentsList.tsx
@@ -1,170 +1,161 @@
+"use client";
 const ApartmentsList = () => {
   return (
-    <>
+    <div className="px-5 py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
       <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: "20px",
-          justifyContent: "center",
-          padding: "20px",
-        }}
+        className="airbnb-embed-frame"
+        data-id="49529825"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
       >
-        <div
-          className="airbnb-embed-frame"
-          data-id="49529825"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
+        <a href="https://www.airbnb.com/rooms/49529825?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/49529825?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
         >
-          <a href="https://www.airbnb.com/rooms/49529825?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/49529825?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · ★4.79 · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
-
-        <div
-          className="airbnb-embed-frame"
-          data-id="925080660358939939"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
-        >
-          <a href="https://www.airbnb.com/rooms/925080660358939939?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/925080660358939939?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · ★4.93 · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
-
-        <div
-          className="airbnb-embed-frame"
-          data-id="49529757"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
-        >
-          <a href="https://www.airbnb.com/rooms/49529757?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/49529757?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · ★4.89 · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
-
-        <div
-          className="airbnb-embed-frame"
-          data-id="49671963"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
-        >
-          <a href="https://www.airbnb.com/rooms/49671963?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/49671963?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · ★4.60 · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
-
-        <div
-          className="airbnb-embed-frame"
-          data-id="49672026"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
-        >
-          <a href="https://www.airbnb.com/rooms/49672026?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/49672026?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · ★4.86 · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
-
-        <div
-          className="airbnb-embed-frame"
-          data-id="49026142"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
-        >
-          <a href="https://www.airbnb.com/rooms/49026142?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/49026142?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
-
-        <div
-          className="airbnb-embed-frame"
-          data-id="49529445"
-          data-view="home"
-          data-hide-price="true"
-          style={{ width: "450px", height: "300px", margin: "auto" }}
-        >
-          <a href="https://www.airbnb.com/rooms/49529445?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
-            View On Airbnb
-          </a>
-          <a
-            href="https://www.airbnb.com/rooms/49529445?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
-            rel="nofollow"
-          >
-            Rental unit in Vlorë · ★4.78 · 1 bedroom · 2 beds · 1 bath
-          </a>
-          <script
-            async
-            src="https://www.airbnb.com/embeddable/airbnb_jssdk"
-          ></script>
-        </div>
+          Rental unit in Vlorë · ★4.79 · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
       </div>
-    </>
+
+      <div
+        className="airbnb-embed-frame"
+        data-id="925080660358939939"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
+      >
+        <a href="https://www.airbnb.com/rooms/925080660358939939?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/925080660358939939?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
+        >
+          Rental unit in Vlorë · ★4.93 · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
+      </div>
+
+      <div
+        className="airbnb-embed-frame"
+        data-id="49529757"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
+      >
+        <a href="https://www.airbnb.com/rooms/49529757?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/49529757?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
+        >
+          Rental unit in Vlorë · ★4.89 · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
+      </div>
+
+      <div
+        className="airbnb-embed-frame"
+        data-id="49671963"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
+      >
+        <a href="https://www.airbnb.com/rooms/49671963?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/49671963?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
+        >
+          Rental unit in Vlorë · ★4.60 · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
+      </div>
+
+      <div
+        className="airbnb-embed-frame"
+        data-id="49672026"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
+      >
+        <a href="https://www.airbnb.com/rooms/49672026?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/49672026?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
+        >
+          Rental unit in Vlorë · ★4.86 · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
+      </div>
+
+      <div
+        className="airbnb-embed-frame"
+        data-id="49026142"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
+      >
+        <a href="https://www.airbnb.com/rooms/49026142?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/49026142?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
+        >
+          Rental unit in Vlorë · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
+      </div>
+
+      <div
+        className="airbnb-embed-frame"
+        data-id="49529445"
+        data-view="home"
+        data-hide-price="true"
+        style={{ width: "450px", height: "300px", margin: "auto" }}
+      >
+        <a href="https://www.airbnb.com/rooms/49529445?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">
+          View On Airbnb
+        </a>
+        <a
+          href="https://www.airbnb.com/rooms/49529445?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget"
+          rel="nofollow"
+        >
+          Rental unit in Vlorë · ★4.78 · 1 bedroom · 2 beds · 1 bath
+        </a>
+        <script
+          async
+          src="https://www.airbnb.com/embeddable/airbnb_jssdk"
+        ></script>
+      </div>
+    </div>
   );
 };
 
