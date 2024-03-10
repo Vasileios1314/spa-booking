562aa9a28682a1858f3b7c1f543ce6738294cfcb
Vasileios1314 <vasilis.bobonis@hotmail.com>
2024-03-10 08:47:44 +0100

    test pub

--- pages/api/route.ts
+++ pages/api/route.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { NEThandler } from ".netlify/functions/triggerEmail";
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,23 +9,31 @@ export default async function handler(
       return res.status(405).json({ message: "Method Not Allowed" });
     }
 
-    const netlifyResponse = await NEThandler(req.body);
+    const { name, email, message } = req.body;
+    const netlifyFunctionURL = `${process.env.NEXT_PUBLIC_NETLIFY_FUNCTIONS_BASE_URL}/triggerEmail`;
 
-    const responseData = JSON.parse(netlifyResponse.body);
+    const response = await fetch(netlifyFunctionURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "netlify-emails-secret": process.env.NETLIFY_EMAILS_SECRET,
+      },
+      body: JSON.stringify({
+        from: process.env.NEXT_PUBLIC_EMAIL_FROM,
+        to: process.env.NEXT_PUBLIC_EMAIL_TO,
+        subject: "New Ticket!!",
+        parameters: { name, email, message },
+      }),
+    });
 
-    if (netlifyResponse.statusCode === 200) {
-      return res
-        .status(200)
-        .json({ message: "Email sent successfully", data: responseData });
-    } else {
-      return res
-        .status(netlifyResponse.statusCode)
-        .json({ message: "Failed to send email" });
-    }
+    return res.status(200).json({
+      message: `Success ${(process.env.NEXT_PUBLIC_URL, process.env.URL)}`,
+      data: { name, email, message },
+    });
   } catch (error) {
     console.error("Error in /api/route:", error);
     return res
       .status(500)
-      .json({ message: "Server error", error: error.toString() });
+      .json({ message: "Server error", error: error.message });
   }
 }
