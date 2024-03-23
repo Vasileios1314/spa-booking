53fe8eb8cedc08a9143542be6d95ff48e6e8c5f9
Vasileios1314 <vasilis.bobonis@hotmail.com>
2024-03-23 12:12:09 +0100

    csp is not working for mobile / deploy

--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
 /** @type {import('next').NextConfig} */
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.airbnb.com https://www.airbnb.nl https://www.google-analytics.com;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' blob: data:;
-  font-src 'self' https://fonts.gstatic.com;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  frame-src 'self' https://example.com; // Adjust this line as needed
-  block-all-mixed-content;
-  upgrade-insecure-requests;
-`.replace(/\n/g, " ");
+// const cspHeader = `
+//   default-src 'self';
+//   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.airbnb.com https://www.airbnb.nl https://www.google-analytics.com;
+//   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+//   img-src 'self' blob: data:;
+//   font-src 'self' https://fonts.gstatic.com;
+//   object-src 'none';
+//   base-uri 'self';
+//   form-action 'self';
+//   frame-ancestors 'none';
+//   frame-src 'self' https://example.com; // Adjust this line as needed
+//   block-all-mixed-content;
+//   upgrade-insecure-requests;
+// `.replace(/\n/g, " ");
 
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader,
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: cspHeader,
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
