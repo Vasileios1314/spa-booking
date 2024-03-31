76402b1d6c55f538d937d34d182816a618e94e4a
Vasileios1314 <vasilis.bobonis@hotmail.com>
2024-04-01 00:26:59 +0200

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,27 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Video from "@/components/Video";
+import Script from "next/script";
 import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-TXKSHGJVSB"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-TXKSHGJVSB');
+      `}
+      </Script>
+
       <Breadcrumb pageName="" description="" />
       <ScrollUp />
       <Hero />
